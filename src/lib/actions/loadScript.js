d0b8e83ddb0c1ac0ebb596397de581e5075f2bae
GhostBoulton <cboulton@ghostery.com>
2017-08-29 14:28:15 -0600

    new types in loadscript

--- src/lib/actions/loadScript.js
+++ src/lib/actions/loadScript.js
@@ -5,52 +5,86 @@ module.exports = function(settings) {
     var getExtensionSettings = require('@turbine/get-extension-settings');
     var extensionSettings = getExtensionSettings();
 
-/* Type:  Link  nid=16102 cid=242 */
-var linkFrag = create('<a id="_bapw-link" href="#" target="_blank" style="text-decoration:none !important"><span style="vertical-align:middle !important">Cookie consent</span></a>');
-document.body.insertBefore(linkFrag, document.body.childNodes[0]);
-
-  (function() {
-    var ev = document.createElement('script'); 
-    ev.type = 'text/javascript'; 
-    ev.async = true; 
-    ev.setAttribute('data-ev-tag-pid', extensionSettings.noticeId); 
-    ev.setAttribute('data-ev-tag-ocid', extensionSettings.companyId); 
-    ev.src = '//c.betrad.com/pub/tag.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ev, s);
-  })();
-/* End:  Link */
-
-
-
-
-
-// (function (id, cid, cb) {
-//     var d = document
-//         , s = d.createElement('script')
-//         , ts = d.getElementsByTagName('script')[0];
-//     s.type = 'text/javascript';
-//     s.async = true;
-//     s.setAttribute('data-ev-noticeid', id);
-//     s.setAttribute('data-ev-coid', cid);
-//     s.setAttribute('data-ev-consent-callback', cb);
-//     s.setAttribute('data-ev-consent-type', 'cn');
-//     s.src = '//c.betrad.com/pub/gdprnotice.js';
-//     ts.parentNode.insertBefore(s, ts);
-// })(extensionSettings.noticeId, extensionSettings.companyId, 'g_consentGiven');
-
+    switch(extensionSettings.type) {
+        case 'in-page': // link
+            (function() {
+                var ev = document.createElement('script'); 
+                ev.type = 'text/javascript'; 
+                ev.async = true; 
+                ev.setAttribute('data-ev-tag-pid', extensionSettings.noticeId); 
+                ev.setAttribute('data-ev-tag-ocid', extensionSettings.companyId); 
+                ev.src = '//c.betrad.com/pub/tag.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ev, s);
+            })();
+            break;
+        case 'hover': // persistent
+            (function (id, cid, cb) {
+                var d = document
+                    , s = d.createElement('script')
+                    , ts = d.getElementsByTagName('script')[0];
+                s.type = 'text/javascript';
+                s.async = true;
+                s.setAttribute('data-ev-noticeid', id);
+                s.setAttribute('data-ev-coid', cid);
+                s.setAttribute('data-ev-consent-callback', cb);
+                s.setAttribute('data-ev-consent-type', 'cn');
+                s.src = '//c.betrad.com/pub/gdprnotice.js';
+                ts.parentNode.insertBefore(s, ts);
+            })(extensionSettings.noticeId, extensionSettings.companyId, 'g_consentGiven');
+            break;
+        case 'explicit': // explicit
+            (function (id, cid, cb) {
+                var d = document
+                    , s = d.createElement('script')
+                    , ts = d.getElementsByTagName('script')[0];
+                s.type = 'text/javascript';
+                s.async = true;
+                s.setAttribute('data-ev-noticeid', id);
+                s.setAttribute('data-ev-coid', cid);
+                s.setAttribute('data-ev-consent-callback', cb);
+                s.setAttribute('data-ev-consent-type', 'cn');
+                s.src = '//c.betrad.com/pub/gdprnotice.js';
+                ts.parentNode.insertBefore(s, ts);
+            })(extensionSettings.noticeId, extensionSettings.companyId, 'g_consentGiven');
+            break;
+        case 'in-page implied': // Link with Implied
+            (function() {
+                var ev = document.createElement('script'); ev.type = 'text/javascript'; ev.async = true; ev.setAttribute('data-ev-tag-pid', extensionSettings.noticeId); ev.setAttribute('data-ev-tag-ocid', extensionSettings.companyId); 
+                ev.src = '//c.betrad.com/pub/tag.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ev, s);
+            })();
+            (function (id, cid, cb) {
+                var d = document
+                    , s = d.createElement('script')
+                    , ts = d.getElementsByTagName('script')[0];
+                s.type = 'text/javascript';
+                s.async = true;
+                s.setAttribute('data-ev-noticeid', id);
+                s.setAttribute('data-ev-coid', cid);
+                s.setAttribute('data-ev-consent-callback', cb);
+                s.setAttribute('data-ev-consent-type', 'cn');
+                s.src = '//c.betrad.com/pub/gdprnotice.js';
+                ts.parentNode.insertBefore(s, ts);
+            })(extensionSettings.noticeId, extensionSettings.companyId, 'g_consentGiven');
+            break;
+        case 'hover implied': // persistent with implied
+        (function (id, cid, cb) {
+            var d = document
+                , s = d.createElement('script')
+                , ts = d.getElementsByTagName('script')[0];
+            s.type = 'text/javascript';
+            s.async = true;
+            s.setAttribute('data-ev-noticeid', id);
+            s.setAttribute('data-ev-coid', cid);
+            s.setAttribute('data-ev-consent-callback', cb);
+            s.setAttribute('data-ev-consent-type', 'cn');
+            s.src = '//c.betrad.com/pub/gdprnotice.js';
+            ts.parentNode.insertBefore(s, ts);
+        })(extensionSettings.noticeId, extensionSettings.companyId, 'g_consentGiven');
+            break;
+        default:
+            //code block of nothingness
+    }
 };
 
-function create(htmlStr) {
-    var frag = document.createDocumentFragment(),
-        temp = document.createElement('div');
-    temp.innerHTML = htmlStr;
-    while (temp.firstChild) {
-        frag.appendChild(temp.firstChild);
-    }
-    return frag;
-}
-
-//var fragment = create('<div>Hello!</div><p>...</p>');
-// You can use native DOM methods to insert the fragment:
-//document.body.insertBefore(fragment, document.body.childNodes[0]);
