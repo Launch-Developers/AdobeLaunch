6d2a3c7b07c6e9b5b92804cca6a7c8900769d58f
GhostBoulton <cboulton@ghostery.com>
2017-08-30 13:34:18 -0600

    removing unused files garbage

--- src/lib/events/consentClick.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(settings, trigger) {
-  document.addEventListener('click', function(event) {
-    if(event.srcElement.id == 'consentButton'){
-      trigger();
-    }
-  });
-};
